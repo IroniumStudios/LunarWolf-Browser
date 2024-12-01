3804105a3a39307c629d2204145c16459570edff
Mr.Anonymous <137374946+IroniumStudios@users.noreply.github.com>
2024-12-01 03:41:03 -0800

    step 1: fixing adblock

--- src/main/services/adblock.ts
+++ src/main/services/adblock.ts
@@ -1,80 +1,80 @@
-/* some elements of this code contains lines from Browser Base and other respective projects, all credit goes to them for there work */
-
-// for what ever reason this code will not work even tho theres no errers,
-// i am currently in the process of working on fixing this adblock code
+/* some elements of this code contain lines from Browser Base and other respective projects, all credit goes to them for their work */
 
 import { existsSync, promises as fs } from 'fs';
 import { resolve, join } from 'path';
 import fetch from 'node-fetch';
 
-import { ElectronBlocker, Request } from '@cliqz/adblocker-electron';
+import { ElectronBlocker, Request } from '@ghostery/adblocker-electron';
 import { getPath } from '~/utils';
 import { Application } from '../application';
-import { ipcMain } from 'electron';
+import { ipcMain, Session } from 'electron';
 
 export let engine: ElectronBlocker;
 
 const PRELOAD_PATH = join(__dirname, './preload.js');
 
 const loadFilters = async () => {
-  const path = resolve(getPath('adblock/cache.dat'));
+  // Fetch and validate the cache path
+  let cachePath = getPath('adblock/cache.dat');
+  
+  // Log the cachePath to understand its format
+  console.log('Cache Path:', cachePath); 
+
+  // If it's a number or other unexpected type, convert it to a string
+  if (typeof cachePath !== 'string') {
+    console.error(`Expected a string for cache path, but received ${typeof cachePath}`);
+    cachePath = String(cachePath);  // Convert it to a string (if not already)
+  }
+  
+  const path = resolve(cachePath);  // Resolve to absolute path
 
   const downloadFilters = async () => {
-    // Load lists to perform ads and tracking blocking:
-    //
-    //  - https://easylist.to/easylist/easylist.txt
-    //  - https://pgl.yoyo.org/adservers/serverlist.php?hostformat=adblockplus&showintro=1&mimetype=plaintext
-    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt
-    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt
-    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt
-    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt
-    //
-    //  - https://easylist.to/easylist/easyprivacy.txt
-    //  - https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt
     engine = await ElectronBlocker.fromPrebuiltAdsAndTracking(fetch);
 
     try {
       await fs.writeFile(path, engine.serialize());
     } catch (err) {
-      if (err) return console.error(err);
+      console.error(err);
     }
   };
 
   if (existsSync(path)) {
     try {
-      const buffer = await fs.readFile(resolve(path));
+      const buffer = await fs.readFile(path);
 
       try {
         engine = ElectronBlocker.deserialize(buffer);
       } catch (e) {
-        return downloadFilters();
+        await downloadFilters();
       }
     } catch (err) {
-      return console.error(err);
+      console.error(err);
     }
   } else {
-    return downloadFilters();
+    await downloadFilters();
   }
 };
 
 const emitBlockedEvent = (request: Request) => {
   const win = Application.instance.windows.findByContentView(request.tabId);
   if (!win) return;
-  win.viewManager.views.get(request.tabId).emitEvent('blocked-ad');
+  const view = win.viewManager.views.get(request.tabId);
+  if (view) {
+    view.emitEvent('blocked-ad');
+  }
 };
 
-  let adblockRunning = false;
-  let adblockInitialized = false;
+let adblockRunning = false;
+let adblockInitialized = false;
 
-  interface IAdblockInfo {
-  headersReceivedId?: number;
-  beforeRequestId?: number;
+interface IAdblockInfo {
+  headersReceivedHandler?: (details: any, callback: any) => void;
+  beforeRequestHandler?: (details: any, callback: any) => void;
 }
 
-const sessionAdblockInfoMap: Map<Electron.Session, IAdblockInfo> = new Map();
+const sessionAdblockInfoMap: Map<Session, IAdblockInfo> = new Map();
 
-
-export const runAdblockService = async (ses: any) => {
+export const runAdblockService = async (ses: Session) => {
   if (!adblockInitialized) {
     adblockInitialized = true;
     await loadFilters();
@@ -90,54 +90,66 @@ export const runAdblockService = async (ses: any) => {
 
   const info = sessionAdblockInfoMap.get(ses) || {};
 
-  if (!info.headersReceivedId) {
-    info.headersReceivedId = ses.webRequest.addListener(
-      'onHeadersReceived',
-      { urls: ['<all_urls>'] },
-      (engine as any).onHeadersReceived,
-      { order: 0 },
-    ).id;
+  // Handlers for web requests
+  const headersReceivedHandler = (details: any, callback: any) => {
+    engine.onHeadersReceived(details, callback);
+  };
+
+  const beforeRequestHandler = (details: any, callback: any) => {
+    engine.onBeforeRequest(details, callback);
+  };
+
+  if (!info.headersReceivedHandler) {
+    ses.webRequest.onHeadersReceived({ urls: ['<all_urls>'] }, headersReceivedHandler);
+    info.headersReceivedHandler = headersReceivedHandler;
   }
 
-  if (!info.beforeRequestId) {
-    info.beforeRequestId = ses.webRequest.addListener(
-      'onBeforeRequest',
-      { urls: ['<all_urls>'] },
-      (engine as any).onBeforeRequest,
-      { order: 0 },
-    ).id;
+  if (!info.beforeRequestHandler) {
+    ses.webRequest.onBeforeRequest({ urls: ['<all_urls>'] }, beforeRequestHandler);
+    info.beforeRequestHandler = beforeRequestHandler;
   }
 
   sessionAdblockInfoMap.set(ses, info);
 
-  ipcMain.on('get-cosmetic-filters', (engine as any).onGetCosmeticFilters);
-  ipcMain.on(
-    'is-mutation-observer-enabled',
-    (engine as any).onIsMutationObserverEnabled,
-  );
-  ses.setPreloads(ses.getPreloads().concat([PRELOAD_PATH]));
+  ipcMain.on('get-cosmetic-filters', (event) => {
+    if (engine && typeof engine.getCosmeticsFilters === 'function') {
+      const filters = engine.getCosmeticsFilters();
+      event.returnValue = filters;
+    }
+  });
+
+  ipcMain.on('is-mutation-observer-enabled', (event) => {
+    if (engine && typeof engine.onIsMutationObserverEnabled === 'function') {
+      const isEnabled = engine.onIsMutationObserverEnabled();
+      event.returnValue = isEnabled;
+    }
+  });
+
+  const preloads = ses.getPreloads();
+  ses.setPreloads([...preloads, PRELOAD_PATH]);
 
   engine.on('request-blocked', emitBlockedEvent);
   engine.on('request-redirected', emitBlockedEvent);
 };
 
-export const stopAdblockService = (ses: any) => {
-  if (!ses.webRequest.removeListener) return;
+export const stopAdblockService = (ses: Session) => {
   if (!adblockRunning) return;
 
-  adblockRunning = true;
+  adblockRunning = false;
 
   const info = sessionAdblockInfoMap.get(ses) || {};
 
-  if (info.beforeRequestId) {
-    ses.webRequest.removeListener('onBeforeRequest', info.beforeRequestId);
-    info.beforeRequestId = null;
+  if (info.beforeRequestHandler) {
+    ses.webRequest.onBeforeRequest({ urls: ['<all_urls>'] }, null);
+    info.beforeRequestHandler = undefined;
   }
 
-  if (info.headersReceivedId) {
-    ses.webRequest.removeListener('onHeadersReceived', info.headersReceivedId);
-    info.headersReceivedId = null;
+  if (info.headersReceivedHandler) {
+    ses.webRequest.onHeadersReceived({ urls: ['<all_urls>'] }, null);
+    info.headersReceivedHandler = undefined;
   }
 
   ses.setPreloads(ses.getPreloads().filter((p: string) => p !== PRELOAD_PATH));
+
+  sessionAdblockInfoMap.delete(ses);
 };
